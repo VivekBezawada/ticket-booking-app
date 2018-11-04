5081854655d259fa75677514e6b916859a8f3904
epskip <bezawadavivek@gmail.com>
2018-11-05 02:40:55 +0530

    get user details by ticket

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const configModel = mongoose.model('Config');
+const bookingModel = mongoose.model('Booking');
 const responseHandler = require('../handlers/responseHandler');
 const TICKET_STATES = ['open','closed'];
 
@@ -59,9 +60,23 @@ exports.getTicketStatus = async (req,res) => {
         if(ticketStatus == TICKET_STATES[0]) {
             return responseHandler.getResponse(200,'There is no person associated with this ticket number',null,res);
         } else {
-            // TODO : Add Users Models and get User Details
+            const bookingData = await bookingModel.findById(configData.seats[ticketNumber-1]).populate('user');
+
+            if(!bookingData) {
+                return responseHandler.getResponse(500, 'Unable to fetch user data',null,res);
+            }
+
+            const userData = {
+                'name'  :   bookingData.user.name,
+                'age'  :   bookingData.user.age,
+                'gender'  :   bookingData.user.gender,
+                'ticketNumber' : ticketNumber,
+                'pnr' : bookingData.pnr
+            }
+
+            return responseHandler.getResponse(200,'Successfully retrieved ticket status',userData,res);
         }
-    }
+    } 
 
     return responseHandler.getResponse(200,'Successfully retrieved ticket status',{status:ticketStatus},res);
 }
