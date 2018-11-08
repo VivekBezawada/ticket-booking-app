c40d63006cea51d78c71cb79678691ef13c725e8
epskip <bezawadavivek@gmail.com>
2018-11-08 11:24:50 +0530

    integration tests on controllers

---
+++ tests/controllers/configController.spec.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Verifying APIs --> Config', function(){
+    it.skip('Reset Tickets using correct credentials returns true', function(done){
+        chai.request('localhost:8081/')
+            .get('/')
+            .end((err,res) => {
+                console.log(res);
+                res.should.have.status(200)
+                done();
+            })
+    })
+})
