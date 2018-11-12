71765737013ad889dc527cd4db2bedebecaaf69d
epskip <bezawadavivek@gmail.com>
2018-11-12 21:45:03 +0530

    tests - booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,72 +14,72 @@ exports.updateTicket = async (req,res) => {
         return responseHandler.getResponse(200, 'Please provide a ticket number',null,res);
     }
 
-    if(ticketNumber < 1 || ticketNumber > 40) {
-        return responseHandler.getResponse(200, 'Enter a valid ticket number',null,res);
-    }
+    if(ticketNumber >= 1 && ticketNumber <= 40) {
+        const configData = await configModel.findOne({});
 
-    const configData = await configModel.findOne({});
-
-    if(!configData) {
-        return responseHandler.getResponse(500, 'Unable to fetch config data',null,res);
-    }
-
-    const ticketStatus = configData.seats[ticketNumber-1] == 0? TICKET_STATES[0] : TICKET_STATES[1];
-    
-    if(ticketStatus == TICKET_STATES[0]) {
-        const userObject = sanitationHandler.verifyUserData(req.body);
-
-        if(!userObject) {
-            return responseHandler.getResponse(500, 'Some fields are missing for User Object',null,res);
+        if(!configData) {
+            return responseHandler.getResponse(500, 'Unable to fetch config data',null,res);
         }
 
-        const writeUserData = await new userModel(userObject).save();
-
-        if(!writeUserData) {
-            return responseHandler.getResponse(500, 'Unable to add user details',null,res);
-        }
-
-        const bookingData = {
-            user : writeUserData._id,
-            ticketNumber : ticketNumber,
-            pnr : randomstring.generate(5)
-        };
-
-        const writeBookingData = await new bookingModel(bookingData).save();
-
-        if(!writeBookingData) {
-            return responseHandler.getResponse(500, 'Unable to add booking details',null,res);
-        }
-
-        configData.seats[ticketNumber-1] = writeBookingData._id;
-        const updateConfigData = await configModel.updateOne({},{$set:{'seats':configData.seats}});
-
-        if(!updateConfigData) {
-            return responseHandler.getResponse(500, 'Unable to update ticket',null,res);
-        }
+        const ticketStatus = configData.seats[ticketNumber-1] == 0? TICKET_STATES[0] : TICKET_STATES[1];
         
-        return responseHandler.getResponse(200,'Successfully updated ticket details',{status:true},res);
+        if(ticketStatus == TICKET_STATES[0]) {
+            const userObject = sanitationHandler.verifyUserData(req.body);
+
+            if(!userObject) {
+                return responseHandler.getResponse(500, 'Some fields are missing for User Object',null,res);
+            }
+
+            const writeUserData = await new userModel(userObject).save();
+
+            if(!writeUserData) {
+                return responseHandler.getResponse(500, 'Unable to add user details',null,res);
+            }
+
+            const bookingData = {
+                user : writeUserData._id,
+                ticketNumber : ticketNumber,
+                pnr : randomstring.generate(5)
+            };
+
+            const writeBookingData = await new bookingModel(bookingData).save();
+
+            if(!writeBookingData) {
+                return responseHandler.getResponse(500, 'Unable to add booking details',null,res);
+            }
+
+            configData.seats[ticketNumber-1] = writeBookingData._id;
+            const updateConfigData = await configModel.updateOne({},{$set:{'seats':configData.seats}});
+
+            if(!updateConfigData) {
+                return responseHandler.getResponse(500, 'Unable to update ticket',null,res);
+            }
+            
+            return responseHandler.getResponse(200,'Successfully updated ticket details',{status:true},res);
+        } else {
+            const query = {ticketNumber : ticketNumber, isActiveBooking : true};
+            const bookingData = await bookingModel.find(query);
+
+            if(!bookingData) {
+                return responseHandler.getResponse(500, 'Booking is not found',null,res);
+            }
+
+            const updateBookingData = await bookingModel.updateOne(query,{$set:{isActiveBooking:false}});
+
+            if(!updateBookingData) {
+                return responseHandler.getResponse(500, 'Unable to update ticket',null,res);
+            }
+            
+            configData.seats[ticketNumber-1] = 0;
+            const updateConfigData = await configModel.updateOne({},{$set:{'seats':configData.seats}});
+
+            if(!updateConfigData) {
+                return responseHandler.getResponse(500, 'Unable to update ticket',null,res);
+            }
+
+            return responseHandler.getResponse(200,'Successfully updated ticket details',{status:true},res);
+        }
     } else {
-        const query = {ticketNumber : ticketNumber, isActiveBooking : true};
-        const bookingData = await bookingModel.find(query);
-
-        if(!bookingData) {
-            return responseHandler.getResponse(500, 'Booking is not found',null,res);
-        }
-
-        const updateBookingData = await bookingModel.updateOne(query,{$set:{isActiveBooking:false}});
-
-        if(!updateBookingData) {
-            return responseHandler.getResponse(500, 'Unable to update ticket',null,res);
-        }
-        
-        configData.seats[ticketNumber-1] = 0;
-        const updateConfigData = await configModel.updateOne({},{$set:{'seats':configData.seats}});
-
-        if(!updateConfigData) {
-            return responseHandler.getResponse(500, 'Unable to update ticket',null,res);
-        }
-
-        return responseHandler.getResponse(200,'Successfully updated ticket details',{status:true},res);
+        return responseHandler.getResponse(200, 'Enter a valid ticket number',null,res);
     }
 }
--- tests/controllers/bookingController.spec.js
+++ tests/controllers/bookingController.spec.js
@@ -4,15 +4,61 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 const CONSTANTS = require('../../constants').CONSTANTS;
 
-describe.skip('Verifying APIs --> Booking APIs', function(){
-    it('Returns true if application is working normally', function(done){
+describe('Verifying APIs --> Booking APIs', function(){
+    it('Returns true for a valid response on update ticket', function(done){
+        const userData = {
+            name    :   'vivek',
+            age     :   23,
+            gender  :   'male'
+        }
         chai.request(CONSTANTS.SERVER)
-            .get('/')
+            .put('/v1/ticket/1')
+            .send(userData)
             .end((err,res) => {
                 expect(err).to.be.null;
                 expect(res).to.have.status(200);
                 expect(res).to.have.property('status');
-                expect(res.body.status).to.be.equal('Application is working normally');
+                expect(res.body).to.have.property('message');
+                expect(res.body).to.have.property('result');
+                expect(res.body.message).to.be.equal('Successfully updated ticket details');
+                expect(res.body.result).to.have.property('status');
+                expect(res.body.result.status).to.be.a('Boolean');
+                done();
+            })
+    });
+
+    it('Returns true for a valid response on update ticket with invalid payload', function(done){
+        const userData = {
+        }
+        chai.request(CONSTANTS.SERVER)
+            .put('/v1/ticket/2')
+            .send(userData)
+            .end((err,res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(500);
+                expect(res).to.have.property('status');
+                expect(res.body).to.have.property('message');
+                expect(res.body).to.have.property('result');
+                expect(res.body.message).to.be.equal('Some fields are missing for User Object');
+                expect(res.body.result).to.be.null;
+                done();
+            })
+    });
+
+    it('Returns true for a valid response on update ticket with invalid ticket number', function(done){
+        const userData = {
+        }
+        chai.request(CONSTANTS.SERVER)
+            .put('/v1/ticket/test')
+            .send(userData)
+            .end((err,res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                expect(res).to.have.property('status');
+                expect(res.body).to.have.property('message');
+                expect(res.body).to.have.property('result');
+                expect(res.body.message).to.be.equal('Enter a valid ticket number');
+                expect(res.body.result).to.be.null;
                 done();
             })
     });
